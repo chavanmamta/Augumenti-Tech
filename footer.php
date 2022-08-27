4823779a58864c74b506b2770174b2d5721dfcb5
chavanmamta <86342331+chavanmamta@users.noreply.github.com>
2022-08-27 07:14:33 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,77 @@
+<!--footer-->
+	<footer>
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-4 footer-grid-agileits-w3ls text-left">
+					<h3>About US</h3>
+					<p><?php getshortdescription("titles"); ?></p>
+					<div class="read">
+						<a href="about.php" class="btn btn-primary read-m">Read More</a>
+					</div>
+				</div>
+				<div class="col-lg-4 footer-grid-agileits-w3ls text-left">
+
+					<div class="tech-btm">
+						<h3>Agumentic tech</h3>
+						<?php getpopularposts("page_hits"); ?>
+					</div>
+				</div>
+				<!-- subscribe -->
+				<div class="col-lg-4 subscribe-main footer-grid-agileits-w3ls text-left">
+					<h2>Signup to our newsletter</h2>
+					<div class="subscribe-main text-left">
+							<div class="subscribe-form">
+									<form action="#" method="post" class="subscribe_form">
+										<input class="form-control" type="email" placeholder="Enter your email..." required="">
+										<button type="submit" class="btn btn-primary submit">Submit</button>
+									</form>
+									<div class="clearfix"> </div>
+						   </div>
+						<p>Welcome To Agumentic tech!</p>
+					</div>
+					<!-- //subscribe -->
+				</div>
+			</div>
+			<!-- footer -->
+			<div class="footer-cpy text-center">
+				<div class="footer-social">
+					<div class="copyrighttop">
+						<ul>
+							<li class="mx-3">
+								<a class="facebook" href="<?php getlinks("links","facebook");?>">
+									<i class="fab fa-facebook-f"></i>
+									<span>Facebook</span>
+								</a>
+							</li>
+							<li>
+								<a class="facebook" href="<?php getlinks("links","twitter");?>">
+									<i class="fab fa-twitter"></i>
+									<span>Twitter</span>
+								</a>
+							</li>
+							<li class="mx-3">
+								<a class="facebook" href="<?php getlinks("links","googleplus");?>">
+									<i class="fab fa-google-plus-g"></i>
+									<span>Google+</span>
+								</a>
+							</li>
+							<li>
+								<a class="facebook" href="<?php getlinks("links","whatsapp");?>">
+									<i class="fab fa-whatsapp"></i>
+									<span>whatsapp</span>
+								</a>
+							</li>
+						</ul>
+
+					</div>
+				</div>
+				<div class="w3layouts-agile-copyrightbottom">
+					<p> Responsive Blog Site <?php $current=date("Y"); print_r($current);?> | Brought To You by
+						<a href="#">Agumentic Tech</a>
+					</p>
+
+				</div>
+			</div>
+			<!-- //footer -->
+		</div>
+	</footer>
